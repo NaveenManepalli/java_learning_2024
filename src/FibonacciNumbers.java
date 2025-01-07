ab9f229e35cd08736f3d5efc404b891438d1f293
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2025-01-07 17:47:47 +0530

    Doc(01/07/2025)

---
+++ src/FibonacciNumbers.java
@@ -0,0 +1,12 @@
+public class FibonacciNumbers {
+    public static void main(String[] args) {
+        int n = 10, first = 0, second = 1;
+        System.out.print("Fibonacci series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
