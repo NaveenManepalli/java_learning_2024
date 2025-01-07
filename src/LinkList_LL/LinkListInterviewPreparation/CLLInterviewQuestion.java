d3ba9a854f2050b9c2e7e48cd8306338669b1230
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2025-01-07 15:53:09 +0530

    LeetCode problems Cyclic Linked list Introduction(01/07/2025)

--- src/LinkList_LL/LinkListInterviewPreparation/CLLInterviewQuestion.java
+++ src/LinkList_LL/LinkListInterviewPreparation/CLLInterviewQuestion.java
@@ -1,6 +1,45 @@
 package LinkList_LL.LinkListInterviewPreparation;
 
+import LinkList_LL.CLL_CircularLinkedList.LL;
+
 public class CLLInterviewQuestion {
+
+
+
+    private ListNode head;
+    private ListNode tail;
+    private int size;
+
+    public void insertFirst(int val) {
+        ListNode node = new ListNode(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        ListNode node = new ListNode(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void display() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
 
 
 
@@ -114,7 +153,7 @@ public class CLLInterviewQuestion {
     }*/
 
     //876.Middle of the linked list
-    public ListNode middleNode(ListNode head){
+    public static ListNode middleNode(ListNode head){
         ListNode s = head;
         ListNode f = head;
         while (f!=null&&f.next!=null){
@@ -125,17 +164,147 @@ public class CLLInterviewQuestion {
 
     //148.Sort List
 
+    class Solution {
+        public ListNode sortList (ListNode head){
+            if(head == null||head.next==null){
+                return head;
+            }
+            ListNode mid = getMid(head);
+            ListNode left = sortList(head);
+            ListNode right =  sortList(mid);
+            return merge(left,right);
+        }
+        ListNode merge(ListNode f, ListNode s) {
+            ListNode DummyHead = new ListNode();
+            ListNode ans =DummyHead;
+            while (f != null && s != null) {
+                if (f.val< s.val) {
+                    ans.next=f;
+                    f = f.next;
+                    ans=ans.next;
+                } else {
+                    ans.next=s;
+                    s = s.next;
+                    ans=ans.next;
+                }
+            }
+            ans.next = (f != null) ? f : s;
+            return DummyHead.next;
+        }
+        ListNode getMid(ListNode head){
+            ListNode preMid = null;
+            while(head!=null && head.next!=null){
+                preMid = (preMid == null)?head:preMid.next;
+                head=head.next.next;
+
+            }
+            ListNode mid =preMid.next;
+            preMid.next=null;
+            return mid;
+        }
+    }
+
+
+
+    // https://leetcode.com/problems/reverse-linked-list/submissions/
+    // google, apple, amazon, microsoft
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
+
+
+    //
+
+    // https://leetcode.com/problems/reorder-list/
+// Google, Facebook
+    public static void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middleNode(head);
+
+        ListNode hs = reverseList(mid);
+        ListNode hf = head;
+
+        // rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+
+        // next of tail to null
+        if (hf != null) {
+            hf.next = null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        CLLInterviewQuestion list = new CLLInterviewQuestion();
+
+
+        list.insertFirst(6);
+        list.insertFirst(5);
+        list.insertFirst(4);
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(1);
+
+        list.display();
+        reorderList(list.head);
+
+        list.display();
+
+
+
+    }
+
 
 }
 class ListNode{
+
+
     int val;
     ListNode next;
+
+
 
     public ListNode(){
 
     }
-    ListNode(int x){
+    public ListNode(int x){
         val=x;
         next=null;
     }
+
+    public ListNode(ListNode next) {
+        this.next = next;
+    }
+
+
+
 }
