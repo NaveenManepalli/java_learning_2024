5bf48dd409cd4361ec3f48ea754ebcdb2c360fce
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2025-01-12 17:22:07 +0530

    Doc(01/12/2025)

---
+++ src/SumExample.java
@@ -0,0 +1,10 @@
+public class SumExample {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = sum(5, 10);
+        System.out.println("Sum: " + result); // Output: Sum: 15
+    }
+}
