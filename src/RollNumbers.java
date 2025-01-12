e95d16e8f2723ccea087abdf9cd3cd263c6053c2
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2025-01-12 22:31:15 +0530

    Doc(01/12/2025)

---
+++ src/RollNumbers.java
@@ -0,0 +1,8 @@
+public class RollNumbers {
+    public static void main(String[] args) {
+        int[] rollNumbers = {101, 102, 103, 104, 105};
+        for (int i = 0; i < rollNumbers.length; i++) {
+            System.out.println("Roll Number: " + rollNumbers[i]);
+        }
+    }
+}
