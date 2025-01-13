3646efc368a0057c128679a6976573e60a8e1c64
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2025-01-13 20:49:25 +0530

    Doc(01/13/2025)

--- src/LinearSearch/EvenDigitsOptimized.java
+++ src/LinearSearch/EvenDigitsOptimized.java
@@ -1,17 +1,27 @@
 package LinearSearch;
 
-public class EvenDigits {
+public class EvenDigitsOptimized {
+    public static int countDigits(int num) {
+        int digits = 0;
+        while (num > 0) {
+            digits++;
+            num /= 10;
+        }
+        return digits;
+    }
+
     public static int countEvenDigits(int[] nums) {
         int count = 0;
         for (int num : nums) {
-            if (String.valueOf(num).length() % 2 == 0) {
+            if (countDigits(num) % 2 == 0) {
                 count++;
             }
         }
         return count;
     }
+
     public static void main(String[] args) {
-        int[] nums = {12, 345, 21, 6, 7892};
+        int[] nums = {12, 345, 2, 6, 7896};
         System.out.println("Count of numbers with even digits: " + countEvenDigits(nums));
-    }//revised
+    }
 }
