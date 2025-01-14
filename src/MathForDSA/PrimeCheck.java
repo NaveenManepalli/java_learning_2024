cb263ff8a959783967e4773b4ea02836f6336c79
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2025-01-14 20:44:37 +0530

    Doc(01/13/2025)

---
+++ src/MathForDSA/PrimeCheck.java
@@ -0,0 +1,16 @@
+package MathForDSA;
+
+public class PrimeCheck {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(17)); // Output: true
+        System.out.println(isPrime(15)); // Output: false
+    }
+}
